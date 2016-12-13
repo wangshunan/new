dcf6fa46ffa2f34c0b19e3c06595705990508fcc
wangshunan <wangshunan12@gmail.com>
2016-12-13 19:33:00 +0900

    光調整

---
+++ New Project/Assets/Player/Script/LightController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+
+	private GameObject Sphere;
+	Color color;
+
+	// Use this for initialization
+	void Start () {
+		Sphere = GameObject.Find("Sphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		color = Sphere.GetComponent<MeshRenderer> ().material.GetColor("_EmissionColor");
+		gameObject.GetComponent<Light> ().color = color;
+	}
+}
