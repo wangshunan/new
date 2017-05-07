69f44576956b99c5ec6e6fc2ff58bf533513c570
wangshunan12 <wangshunan12@gmail.com>
2017-05-08 07:19:29 +0900

    Demo

---
+++ New Project/Assets/lightcontroller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightcontroller : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject Img;
+    public Light imgLight;
+
+    // Use this for initialization
+    void Awake() {
+  }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if ( Input.GetMouseButton(1) )
+        {
+            var pos = Input.mousePosition;
+            pos.z = 5;
+            transform.position = Camera.main.ScreenToWorldPoint(pos);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -0.1f);
+        } else
+        {
+            transform.position = player.transform.position;
+            transform.position = new Vector3(transform.position.x, transform.position.y, -0.1f);
+        }
+        float distance = Vector3.Distance(transform.position, Img.transform.position);
+        if ( distance <= 1 )
+        {   if (imgLight.range < 15)
+            {
+                imgLight.range += 0.1f;
+            }
+        }
+       
+    }
+}
---
+++ New Project/Assets/playercontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    private Vector3 screenToWorldPointPosition;
+
+    float speed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        screenToWorldPointPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition.z = 5;
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            screenToWorldPointPosition.y = 0;
+            screenToWorldPointPosition.z = 0;
+        }
+
+        gameObject.transform.position = Vector3.MoveTowards(transform.position, screenToWorldPointPosition, speed);
+        
+    }
+}
